2cb019fd579e8ef3713ec75e51d46601188386c3
iphone5solo <499491531@qq.com>
2016-06-05 21:02:16 +0800

    Update README.md
    Update README.md

--- PYTableViewController/Other/PYConst.h
+++ PYTableViewController/Other/PYConst.h
@@ -8,7 +8,7 @@
 #define PYColor(r,g,b) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:1.0]
 #define PYRandomColor  PYColor(arc4random_uniform(256),arc4random_uniform(256),arc4random_uniform(256))
 
-// 屏幕狂爱
+// 屏幕宽高
 #define PYScreenW [UIScreen mainScreen].bounds.size.width
 #define PYScreenH [UIScreen mainScreen].bounds.size.height
 
